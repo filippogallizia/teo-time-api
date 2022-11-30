e8229ce216e05de8103ed64331717cfd5ee59a1b
filippo gallizia <galliziafilippo@gmail.com>
2022-11-30 17:16:31 +0000

    Add relation many to many btw Weekly_availability and Event ( through hours )

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,10 @@
 class Event < ApplicationRecord
-  belongs_to :weekly_availability, optional: true
   belongs_to :trainer, :class_name => 'User', :foreign_key => "trainer_id", optional: true
   has_many :bookings
   has_many :hours
+  has_many :weekly_availability, through: :hours
   validates_presence_of :name, :weekly_availability_id, :trainer_id, :duration, :increment_amount
+  after_commit :remove_event_from_hours
 
   def create_avail_slot
     id = read_attribute(self.id)
@@ -21,5 +22,12 @@ class Event < ApplicationRecord
   def hours_with_date_or_wday (date, day_id)
     hours_with_date = self.hours.where(date: date)
     hours_with_date.length > 0 ? hours_with_date : self.hours.where(day_id: day_id, date: nil)
+  end
+
+  def remove_event_from_hours
+    #TODO create test
+    if self.previous_changes["weekly_availability_id"]
+      self.hours&.where(weekly_availability_id: self.weekly_availability_id)&.update(event_id: nil)
+    end
   end
 end
--- app/models/weekly_availability.rb
+++ app/models/weekly_availability.rb
@@ -2,7 +2,7 @@ class WeeklyAvailability < ApplicationRecord
   has_many :hours, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_many :days, through: :hours
-  has_many :events, dependent: :destroy
+  has_many :events, through: :hours
   include TimeHelper
 
 end
